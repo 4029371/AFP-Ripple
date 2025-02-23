f5618b8dae71246be83dd9899c58c5fde80ac20d
Donovan <donovanongty@gmail.com>
2025-02-24 10:28:00 +1100

    Initial Commit

---
+++ AFP-Ripple/AFP_RippleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFP_RippleApp.swift
+//  AFP-Ripple
+//
+//  Created by Donovan Ong on 24/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct AFP_RippleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
